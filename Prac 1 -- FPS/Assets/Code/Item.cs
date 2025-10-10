a50e026db007c5a4ed9034a9837033937b9d4b52
Arunau333 <acatai@edu.tecnocampus.cat>
2025-10-10 15:46:57 +0200

    Update Item.cs

--- Prac 1 -- FPS/Assets/Code/Item.cs
+++ Prac 1 -- FPS/Assets/Code/Item.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
-public class Item : MonoBehaviour
+public abstract class Item : MonoBehaviour 
 {
 
     public virtual void Pick()
     {
         GameObject.Destroy(gameObject);
     }
+    public abstract bool CanPick();
 
 
 
-
+    //Abstract: No se pueden instaciar objetos de esta clase, solo estan definidos.
+    //Protegido:
 }
 
 
