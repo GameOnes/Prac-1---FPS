b47091b47baf2722af0d1d4c159cd9e51c2cd674
Arunau333 <acatai@edu.tecnocampus.cat>
2025-10-10 17:22:55 +0200

    Update EnemyController.cs

--- Prac 1 -- FPS/Assets/Code/EnemyController.cs
+++ Prac 1 -- FPS/Assets/Code/EnemyController.cs
@@ -59,76 +59,91 @@ public class EnemyController : MonoBehaviour
                 UpdateDieState();
                 break;
         }
-        void SetIdleState()
-        {
-            m_State = TState.IDLE;
-        }
-        void UpdateIdleState()
-        {
-        }
-        void SetPatrolState()
-        {
-            m_State = TState.PATROL;
-        }
-        void UpdatePatrolState()
-        {
-        }
-        void SetAlertState()
-        {
-            m_State = TState.ALERT;
-        }
-        void UpdateAlertState()
-        {
-        }
-        void SetAttackState()
-        {
-            m_State = TState.ATTACK;
-        }
-        void UpdateAttackState()
-        {
-        }
-        void SetChaseState()
-        {
-            m_State = TState.CHASE;
-        }
-        void UpdateChaseState()
-        {
-        }
-        void SetHitState()
-        {
-            m_State = TState.PATROL;
-        }
-        void UpdateHitState()
-        {
-        }
-        void SetDieState()
-        {
-            m_State = TState.PATROL;
-        }
-        void UpdateDieState()
-        {
-        }
-        public float m_MinDistanceToAttack = 5.0f;
-        void SetNextChasePosition()
-        {
-         Vector3 l_PlayerPosition = GameManager.GetGameManager().GetPlayer().transform.position;
-         Vector3 l_Direction = l_PlayerPosition - transform.position;
-         l_Direction.Normalize();
-         Vector3 l_Position = l_PlayerPosition-l_Direction*m_MinDistanceToAttack;
-         m_NavMeshAgent.destination = l_Position;
-        }
-
-        void MoveToNextPatrolPosition()
-        {
-            Vector3 l_Destination = m_PatrolPositions[m_CurrentPatrolPositionId].position;
-            m_NavMeshAgent.destination = l_Destination;
-            ++m_CurrentPatrolPositionId;
-            if (m_CurrentPatrolPositionId > m_PatrolPositions.Count)
-            {
-                m_CurrentPatrolPositionId = 0;
-            }
-        }
-
-
     }
+    void SetIdleState()
+    {
+        m_State = TState.IDLE;
+    }
+    void UpdateIdleState()
+    {
+    }
+    void SetPatrolState()
+    {
+        m_State = TState.PATROL;
+    }
+    void UpdatePatrolState()
+    {
+    }
+    void SetAlertState()
+    {
+        m_State = TState.ALERT;
+    }
+    void UpdateAlertState()
+    {
+    }
+    void SetAttackState()
+    {
+        m_State = TState.ATTACK;
+    }
+    void UpdateAttackState()
+    {
+    }
+    void SetChaseState()
+    {
+        m_State = TState.CHASE;
+    }
+    void UpdateChaseState()
+    {
+    }
+    void SetHitState()
+    {
+        m_State = TState.PATROL;
+    }
+    void UpdateHitState()
+    {
+    }
+    void SetDieState()
+    {
+        m_State = TState.PATROL;
+    }
+    void UpdateDieState() 
+    {
+    }
+
+    public float m_MinDistanceToAttack = 5.0f;
+    void SetNextChasePosition()
+    {
+        Vector3 l_PlayerPosition = GameManager.GetGameManager().GetPlayer().transform.position;
+        Vector3 l_Direction = l_PlayerPosition - transform.position;
+        l_Direction.Normalize();
+        Vector3 l_Position = l_PlayerPosition-l_Direction*m_MinDistanceToAttack;
+        m_NavMeshAgent.destination = l_Position;
+    }
+    /*
+     Crearemos el método MoveToNextPatrolposition dónde establecerá la posición de destino del Patrol en el NavMeshAgent,
+     para ello utilizaremos una lista de puntos Patrulla para cada enemigo.
+     */
+    void MoveToNextPatrolPosition()
+    {
+        Vector3 l_Destination = m_PatrolPositions[m_CurrentPatrolPositionId].position;
+        m_NavMeshAgent.destination = l_Destination;
+        ++m_CurrentPatrolPositionId;
+        if (m_CurrentPatrolPositionId > m_PatrolPositions.Count)
+        {
+            m_CurrentPatrolPositionId = 0;
+        }
+    }
+    /*
+     Crearemos el método SeesPlayer que nos devolverá verdadero o falso si vemos al player, para ello deberemos
+    utilizar un Raycast entre "los ojos" del player y la posición del enemigo, encaso de colisionar con le enemigo que
+    decidir que ve al enemigo, en caso de colisionar con otro objeto que no sea el enemigo
+    querrá decir que no ve al player.Podemos utilizar una capa para el player para comprobar que no colosione con nadie
+    Deberemos comprobar tambien que el enemigo esta mirando en la direccion del player, para ello utilizaremos el 
+    producto escalar entre dos vectores.
+     */
+
+
 }
+    
+
+
