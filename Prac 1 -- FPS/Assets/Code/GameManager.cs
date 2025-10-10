ac76af9529e7f2dc088ba2582886c17f84d938a9
Arunau333 <acatai@edu.tecnocampus.cat>
2025-10-10 18:54:15 +0200

    .

--- Prac 1 -- FPS/Assets/Code/GameManager.cs
+++ Prac 1 -- FPS/Assets/Code/GameManager.cs
@@ -30,6 +30,7 @@ public class GameManager : MonoBehaviour
         for(int i=0; i < m_DestroyObjects.childCount; i++)
         {
             GameObject.Destroy(m_DestroyObjects.GetChild(i).gameObject);
+            //m_Player.Restart();
         }
         
     }
