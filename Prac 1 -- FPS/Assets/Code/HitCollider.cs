019fae4ba29047186e9b5c2e9ac0738fbfa71719
GameOnes <ialcaide@edu.tecnocampus.cat>
2025-10-17 16:37:45 +0200

    mucho texto

--- Prac 1 -- FPS/Assets/Code/HitCollider.cs
+++ Prac 1 -- FPS/Assets/Code/HitCollider.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class HitCollider : MonoBehaviour
 {
     public int m_Damage;
+    public PlayerController m_Player;
     public EnemyController m_Enemy;
 
     public void Hit()
     {
         m_Enemy.Hit(m_Damage);
+        m_Player.AddScore(); 
     }
 }
