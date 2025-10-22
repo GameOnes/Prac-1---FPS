cf9d9dcf349f30aafc2666b3594a041caf271278
Arunau333 <acatai@edu.tecnocampus.cat>
2025-10-22 17:55:10 +0200

    Shield UI

--- Prac 1 -- FPS/Assets/Code/AmmoItem.cs
+++ Prac 1 -- FPS/Assets/Code/AmmoItem.cs
@@ -7,8 +7,13 @@ public class AmmoItem : Item
     public int m_AmmoCount;
     public override void Pick()
     {
-        base.Pick();
-        GameManager.GetGameManager().GetPlayer().AddAmmo(m_AmmoCount);
+
+        if (CanPick())
+        {
+            base.Pick();
+            GameManager.GetGameManager().GetPlayer().AddAmmo(m_AmmoCount);
+        }
+
     }
     public override bool CanPick()
     {
